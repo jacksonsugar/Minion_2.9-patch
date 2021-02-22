efebd4cd7cd6da811b894c457d0c147a433aed92
Jackson Sugar <33731112+jacksonsugar@users.noreply.github.com>
2021-02-22 13:10:36 -0500

    Update Minion_2.9-patch.py

--- Minion_2.9-patch.py
+++ Minion_2.9-patch.py
@@ -5,7 +5,7 @@ ini_dir = os.getcwd()
 
 # Remove old files
 print('Removing old files...')
-os.system('sudo rm -r /home/pi/Desktop/Minion_config.ini /home/pi/Documents/Minion_scripts/minsat.py /home/pi/Documents/Minion_scripts/Minion_DeploymentHandler.py /home/pi/Documents/Minion_scripts/TempPres.py /home/pi/Documents/Minion_scripts/Recovery_Sampler.py /home/pi/Documents/Minion_scripts/Extended_Sampler.py')
+os.system('sudo rm -r /home/pi/Desktop/Minion_config.ini /home/pi/Documents/Minion_scripts/Iridium_gps.py /home/pi/Documents/Minion_scripts/minsat.py /home/pi/Documents/Minion_scripts/Minion_DeploymentHandler.py /home/pi/Documents/Minion_scripts/TempPres.py /home/pi/Documents/Minion_scripts/Recovery_Sampler.py /home/pi/Documents/Minion_scripts/Extended_Sampler.py')
 
 os.system('sudo rm -r /home/pi/Documents/Minion_scripts/TempPres_IF.py')
 
@@ -15,7 +15,7 @@ print('Replacing new files...')
 
 os.system('sudo cp updates/Minion_config.ini /home/pi/Desktop/Minion_config.ini')
 
-os.system('sudo cp updates/minsat.py updates/Minion_2.9_Systemtest.py updates/Minion_DeploymentHandler.py updates/Recovery_Sampler.py updates/Extended_Sampler.py updates/TempPres.py /home/pi/Documents/Minion_scripts/')
+os.system('sudo cp updates/Iridium_gps.py updates/minsat.py updates/Minion_2.9_Systemtest.py updates/Minion_DeploymentHandler.py updates/Recovery_Sampler.py updates/Extended_Sampler.py updates/TempPres.py /home/pi/Documents/Minion_scripts/')
 
 os.system('sudo mkdir /home/pi/Documents/drivers')
 # Clone repos
